497bcd45c2605fc1dd953ebfc9adeb8dd81dfc4f
Karlson Drendel <kdrendel99@gmail.com>
2021-03-08 20:59:05 -0800

    Build fight functionality
    Co-authored-by: Nick Hennessy <njhnny@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+function Character(name, level, intelligence, strength, charisma, inventory) {
+  this.name = name;
+  this.level = level;
+  this.intelligence = intelligence;
+  this.strength = strength;
+  this.charisma = charisma;
+  this.inventory = [];
+
+};
+
+let newCharacter = new Character("nick", 20, 10000, 9999999, 9999);
+
+console.log(newCharacter);
