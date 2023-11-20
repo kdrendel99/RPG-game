2bc6ac29e56f6f3974ffa6dac50c8fc407d53601
Karlson Drendel <kdrendel99@gmail.com>
2023-11-19 16:10:49 -0800

    Save changes

--- src/js/GameFunctions.js
+++ src/js/GameFunctions.js
@@ -181,19 +181,19 @@ function checkIfPlayerCanHeal(thisPlayer, player){
 }
 
 function walkOrBattleRandomizer(randomMonster, player, thisPlayer){
-  // let roll = Math.floor(Math.random() * (3-1) + 1);
-  // if (roll === 1){
+  let roll = Math.floor(Math.random() * (3-1) + 1);
+  if (roll === 1){
     printTerminal("As you walked, you bumped into an enemy! Quick, prepare for battle!");
     refreshMonsterStats(randomMonster);
     showAttack();
     $('.enemy').fadeIn('slow');
-  // }
-  // else {
-  //   printTerminal("You walked 1 mile. It was uneventful.");
-  //   player(advance);
-  //   rattle('playerProgressRatle','heal');
-  //   refreshPlayerStats(player);
-  // }
+  }
+  else {
+    printTerminal("You walked 1 mile. It was uneventful.");
+    player(advance);
+    rattle('playerProgressRatle','heal');
+    refreshPlayerStats(player);
+  }
 }
 function attackRollOne(player, monster){
   //simple damage to monster
